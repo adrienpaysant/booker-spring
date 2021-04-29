5bb71aa8cfbd0660bca6d36ed40e169ee1a819d7
Paysant Adrien <adrien.paysant@he-arc.ch>
2021-04-29 02:04:08 +0200

    edit pom.xml + add @ into User model

--- Booker/src/main/java/ch/hearc/qdljee/model/User.java
+++ Booker/src/main/java/ch/hearc/qdljee/model/User.java
@@ -1,6 +1,5 @@
 package ch.hearc.qdljee.model;
 
-
 import java.util.Collection;
 
 import javax.persistence.CascadeType;
@@ -10,44 +9,47 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
-import javax.persistence.JoinColumn;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 @Entity
-@Table(name =  "user", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
+@Table(name = "user", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
 public class User {
-	
+
 	@Id
-	@GeneratedValue(strategy =  GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
+	@NotEmpty
+	@Size(min = 3)
 	@Column(name = "first_name")
 	private String firstName;
-	
+
+	@NotEmpty
+	@Size(min = 3)
 	@Column(name = "last_name")
 	private String lastName;
-	
+
+	@Email(message = "Email must be a valid email address.")
 	private String email;
-	
+
 	private String password;
-	
+
 	@ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-	@JoinTable(
-			name = "users_roles",
-			joinColumns = @JoinColumn(
-		            name = "user_id", referencedColumnName = "id"),
-			inverseJoinColumns = @JoinColumn(
-				            name = "role_id", referencedColumnName = "id"))
-	
+	@JoinTable(name = "users_roles", joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+
 	private Collection<Role> roles;
-	
+
 	public User() {
-		
+
 	}
-	
+
 	public User(String firstName, String lastName, String email, String password, Collection<Role> roles) {
 		super();
 		this.firstName = firstName;
@@ -56,39 +58,51 @@ public class User {
 		this.password = password;
 		this.roles = roles;
 	}
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public Collection<Role> getRoles() {
 		return roles;
 	}
+
 	public void setRoles(Collection<Role> roles) {
 		this.roles = roles;
 	}
