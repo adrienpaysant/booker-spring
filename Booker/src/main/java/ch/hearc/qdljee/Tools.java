ded5f27e5e8b2a0a2671e0c1fb5cf7dc7d106038
Paysant Adrien <adrien.paysant@he-arc.ch>
2021-04-29 02:28:20 +0200

    add email validator & fname/lname lenght validation

--- Booker/src/main/java/ch/hearc/qdljee/Tools.java
+++ Booker/src/main/java/ch/hearc/qdljee/Tools.java
@@ -1,10 +1,14 @@
 package ch.hearc.qdljee;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import ch.hearc.qdljee.model.User;
 import ch.hearc.qdljee.service.ShopeUserDetails;
+
 
 public class Tools {
 
@@ -13,4 +17,19 @@ public class Tools {
 		ShopeUserDetails sUDetails = (ShopeUserDetails) auth.getPrincipal();
 		return sUDetails.getUser();
 	}
+
+	public static boolean isValidEmail(String email) {
+		
+		final String EMAIL_REGEX = "^[\\w-\\+]+(\\.[\\w]+)*@[\\w-]+(\\.[\\w]+)*(\\.[a-z]{2,})$";
+		Pattern pattern;
+
+		// non-static Matcher object because it's created from the input String
+		Matcher matcher;
+		// initialize the Pattern object
+		pattern = Pattern.compile(EMAIL_REGEX, Pattern.CASE_INSENSITIVE);
+		matcher = pattern.matcher(email);
+		return matcher.matches();
+	}
+
 }
+
