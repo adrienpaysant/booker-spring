420d8c5577cb3b412c7d29775ce3fe5b59638a42
Paysant Adrien <adrien.paysant@he-arc.ch>
2021-05-01 03:27:48 +0200

    model comments done & comments repository

--- Booker/src/main/java/ch/hearc/qdljee/model/Comments.java
+++ Booker/src/main/java/ch/hearc/qdljee/model/Comments.java
@@ -1,5 +1,66 @@
 package ch.hearc.qdljee.model;
 
+import java.sql.Date;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Table(name = "comments", uniqueConstraints = @UniqueConstraint(columnNames = "id"))
 public class Comments {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	@NotNull
+	@ManyToOne(cascade = { CascadeType.REFRESH, CascadeType.MERGE }, fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
+	private User author;
+
+	private Integer bookId;
+	@DateTimeFormat(pattern = "dd/MM/yyyy")
+	private Date publicationDate;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public User getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(User author) {
+		this.author = author;
+	}
+
+	public Integer getBookId() {
+		return bookId;
+	}
+
+	public void setBookId(Integer bookId) {
+		this.bookId = bookId;
+	}
+
+	public Date getPublicationDate() {
+		return publicationDate;
+	}
+
+	public void setPublicationDate(Date publicationDate) {
+		this.publicationDate = publicationDate;
+	}
 
 }
