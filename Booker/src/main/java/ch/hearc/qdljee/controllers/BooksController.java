ed34bf6d509467fca0cd79f2d039669b909948a0
Monnet Joris <joris.monnet@he-arc.ch>
2021-05-01 15:05:25 +0200

    fix some designs

--- Booker/src/main/java/ch/hearc/qdljee/controllers/BooksController.java
+++ Booker/src/main/java/ch/hearc/qdljee/controllers/BooksController.java
@@ -16,6 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
