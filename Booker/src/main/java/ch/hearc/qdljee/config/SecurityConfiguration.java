9f96a9c20b699de46f89e997f558d69f21556b70
Monnet Joris <joris.monnet@he-arc.ch>
2021-05-01 05:53:06 +0200

    fix login and  register style, remove old bootstrap

--- Booker/src/main/java/ch/hearc/qdljee/config/SecurityConfiguration.java
+++ Booker/src/main/java/ch/hearc/qdljee/config/SecurityConfiguration.java
@@ -46,7 +46,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 				 "/register**",
 	                "/js/**",
 	                "/css/**",
-	                "/img/**").permitAll()
+	                "/img/**",
+	                "/webjars/**").permitAll()
 		.anyRequest().authenticated()
 		.and()
 		.formLogin()
